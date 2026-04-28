d5ccf62eab16c29dab60d6f8cac782729fe7ea7c
Pedro Henrique Ferreira Maia <103214986+pedromaiadev@users.noreply.github.com>
2026-04-27 21:45:41 -0300

    teste

--- testtodo.py
+++ testtodo.py
@@ -6,7 +6,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
  
 import todo
  
- 
 def setup_function():
     """Limpa a lista de tarefas antes de cada teste."""
     todo.tarefas.clear()
